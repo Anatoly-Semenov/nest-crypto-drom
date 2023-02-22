2a7af694878119979203e16b0d2a66d7c98cd54f
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-02-22 16:27:30 +0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserModule } from '../user/user.module';
