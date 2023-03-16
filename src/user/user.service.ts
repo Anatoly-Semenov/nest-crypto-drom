5eac831097ba1ea3d53f44550fbe6edce1ef9acb
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-03-16 16:48:00 +0300

    Refactoring dto's in user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+
+import { UpdateUserDto, CreateUserDto } from './dto/';
+import { User } from './entities/user.entity';
 import { UserInterface } from './types/user.interface';
-import { CreateUserDto } from './dto/create-user.dto';
 
 export type UserId = string | number;
 
