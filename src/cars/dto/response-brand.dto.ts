21b57dfc648c58f11889e52e133eccd33e3e0e6b
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-03-16 16:31:46 +0300

    Update dto's in cars module

--- src/cars/dto/response-brand.dto.ts
+++ src/cars/dto/response-brand.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+
 export class ResponseBrandDto {
+  @ApiProperty()
+  @Type(() => Number)
   id: number;
 
+  @ApiProperty()
+  @Type(() => String)
   name: string;
 
   constructor(partial?: Partial<ResponseBrandDto>) {
