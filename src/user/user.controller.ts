a029740c44a4961826e8a1867009752908785107
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-03-16 16:50:56 +0300

    Add swagger documentation to user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,42 +7,71 @@ import {
   Patch,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UserService } from './user.service';
 
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto, ResponseUserDto } from './dto';
 import { GetUser } from './decorators/get-user.decorator';
 
 @Controller('user-service')
+@ApiTags('user-service')
+@ApiBearerAuth()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get('users')
+  @ApiResponse({
+    status: 200,
+    description: 'Get users list.',
+    type: ResponseUserDto,
+    isArray: true,
+  })
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.userService.getAllUsers();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('users/me')
+  @ApiResponse({
+    status: 200,
+    description: 'Get my user data.',
+    type: ResponseUserDto,
+  })
+  @UseGuards(JwtAuthGuard)
   findMe(@GetUser() user) {
     return this.userService.getMe(user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('users/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Get detail user data.',
+    type: ResponseUserDto,
+  })
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.userService.getUserById(+id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch('users/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Update user data by id.',
+    type: ResponseUserDto,
+  })
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
     return this.userService.updateUserById(+id, UpdateUserDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('users/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'Delete user.',
+    type: ResponseUserDto,
+  })
+  @UseGuards(JwtAuthGuard)
   delete(@Param('id') id: string) {
     return this.userService.deleteUserById(+id);
   }
